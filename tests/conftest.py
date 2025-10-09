1e0bf6f853fb80980789c7369fba41616997ef50
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-10 04:27:00 +0900

    test: add guard to test only inside sandbox

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,18 @@ import pytest
 from winconfig.powershell.process import PowershellRunspace
 
 
+@pytest.fixture(autouse=True, scope="session")
+def ensure_sandbox(powershell: PowershellRunspace):
+    is_sandbox = (
+        powershell.run(
+            '((Get-WmiObject Win32_ComputerSystem).Model -eq "Virtual Machine")'
+        )
+        == "True"
+    )
+    if not is_sandbox:
+        pytest.fail("This test must be run inside a Windows Sandbox")
+
+
 @pytest.fixture(scope="session")
 def powershell() -> PowershellRunspace:
     return PowershellRunspace()
