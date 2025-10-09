2c9a2e5e7aa9e4b46ff79ff86410b4a6db1557d7
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-09 12:44:44 +0900

    test: run pytest in wsb by host cli

--- tests/run_wsb.ps1
+++ tests/run_wsb.ps1
@@ -1,14 +1,14 @@
-param([string]$Headless = $true)
+param([string]$Headless = $false)
 
 [bool]$isHeadless = $false
-[bool]::TryParse($val, [ref]$isHeadless) | Out-Null
+[bool]::TryParse($Headless, [ref]$isHeadless) | Out-Null
 
 $runAs = $(if ($isHeadless) { "System" } else { "ExistingLogin" })
 
 wsb stop --id (wsb list)
 echo "Opening sandbox ${id}"
 $id = (wsb start --raw | ConvertFrom-Json).Id
-wsb share --id $id --host-path . --sandbox-path %UserProfile%\winconfig --allow-write
+wsb share --id $id --host-path . --sandbox-path C:\winconfig-readonly
 
 if (!$isHeadless) {
     echo "Logging into sandbox"
@@ -18,9 +18,23 @@ if (!$isHeadless) {
     } while (!$?)
 }
 
-echo "Installing uv"
+$command = "robocopy C:\winconfig-readonly C:\winconfig /s /xf .* /xd .*"
+echo "> $command"
+wsb exec --id $id --command $command --run-as $runAs
+echo "*exit code 1: OK"
+
 $command = "powershell -ExecutionPolicy ByPass -c """"irm https://astral.sh/uv/install.ps1 | iex"""""
+echo "> $command"
 wsb exec --id $id --command $command --run-as $runAs
 
-# $command = "uv run pytest %UserProfile%/winconfig"
-# wsb exec --id $id --command $command --run-as $runAs
+$command = "uv venv --python 3.13"
+echo "> $command"
+wsb exec --id $id --command $command --run-as $runAs --working-directory C:\winconfig
+
+$command = "uv sync"
+echo "> $command"
+wsb exec --id $id --command $command --run-as $runAs --working-directory C:\winconfig
+
+$command = "uv run pytest"
+echo "> $command"
+wsb exec --id $id --command $command --run-as $runAs --working-directory C:\winconfig
