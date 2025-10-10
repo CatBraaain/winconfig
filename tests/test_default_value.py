5dfef7eff9a1eae757a54a518c1eedea1edf870f
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-10 12:56:01 +0900

    refactor: improve readability

--- tests/test_default_value.py
+++ tests/test_default_value.py
@@ -13,9 +13,9 @@ def test_default_resitory(powershell: PowershellRunspace, definition: Definition
     for registry in definition.registries:
         script = ScriptGenerator.generate_get_registry_script(registry)
         current_value = powershell.run(script)
-
-        assert str(current_value) == str(registry.old_value), (
-            f"[{registry.path.replace('Registry::', '')}\\{registry.name}]'s value '{current_value}' != '{registry.old_value}'"
+        expected_value = registry.old_value
+        assert str(current_value) == str(expected_value), (
+            f"[{registry.path.replace('Registry::', '')}\\{registry.name}]'s value '{current_value}' != '{expected_value}'"
         )
 
 
@@ -23,9 +23,9 @@ def test_default_scheduled_task(powershell: PowershellRunspace, definition: Defi
     for task in definition.scheduled_tasks:
         script = ScriptGenerator.generate_get_schtask_script(task)
         current_state = powershell.run(script)
-
-        assert current_state == "<NotExist>" or current_state == task.old_state, (
-            f"[{task.path}]'s state '{current_state}' != '{task.old_state}'"
+        expected_state = task.old_state
+        assert current_state == "<NotExist>" or current_state == expected_state, (
+            f"[{task.path}]'s state '{current_state}' != '{expected_state}'"
         )
 
 
@@ -33,7 +33,7 @@ def test_default_service(powershell: PowershellRunspace, definition: Definition)
     for service in definition.services:
         script = ScriptGenerator.generate_get_service_script(service)
         current_type = powershell.run(script)
-
-        assert (
-            current_type == "<NotExist>" or current_type == service.old_startup_type
-        ), f"[{service.name}]'s type '{current_type}' != '{service.old_startup_type}'"
+        expected_type = service.old_startup_type
+        assert current_type == "<NotExist>" or current_type == expected_type, (
+            f"[{service.name}]'s type '{current_type}' != '{expected_type}'"
+        )
