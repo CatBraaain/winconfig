76c1e3f3f196642ae3e08f8cf3fb5b046c63464f
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-11 14:11:11 +0900

    feat: add preload_scripts to definition

--- src/winconfig/model/definition.py
+++ src/winconfig/model/definition.py
@@ -126,6 +126,7 @@ class Definition(BaseModel):
 
 class DefinitionContainer(BaseModel):
     definitions: list[Definition] = []
+    preload_scripts: list[str] = []
 
     def get_definition(self, task_name: str) -> Definition:
         definition = next((x for x in self.definitions if x.name == task_name), None)
