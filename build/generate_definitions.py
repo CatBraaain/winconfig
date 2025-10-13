c408bec523a17b7f6d4173b297eae5c54b8b8c6b
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-13 10:21:05 +0900

    chore: adjust winutil preload

--- build/generate_definitions.py
+++ build/generate_definitions.py
@@ -9,8 +9,8 @@ def create_winutil_definitions() -> None:
     winutil = WinutilDefinitionContainer.from_winutil_url(
         definition_url="https://raw.githubusercontent.com/ChrisTitusTech/winutil/refs/heads/main/config/tweaks.json",
         preload_script_urls=[
-            "https://raw.githubusercontent.com/ChrisTitusTech/winutil/refs/heads/main/functions/public/Invoke-WPFTweakPS7.ps1",
             "https://raw.githubusercontent.com/ChrisTitusTech/winutil/refs/heads/main/functions/private/Invoke-WinUtilExplorerUpdate.ps1",
+            "https://raw.githubusercontent.com/ChrisTitusTech/winutil/refs/heads/main/functions/public/Invoke-WPFRunspace.ps1",
         ],
     )
     winutil.output_yaml_file("src/winconfig/definitions/winutil_definition.yaml")
