6708def2954d61773606a405006a84f6f6a6a02b
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-12-02 14:03:40 +0900

    test: avoid uv blocked by SmartAppControl

--- tests/run_test_in_wsb.ps1
+++ tests/run_test_in_wsb.ps1
@@ -18,6 +18,14 @@ if (!$isHeadless) {
     } while (!$?)
 }
 
+$command = "reg add ""HKLM\SYSTEM\CurrentControlSet\Control\CI\Policy"" /v VerifiedAndReputablePolicyState /t REG_DWORD /d 0 /f"
+echo "> $command"
+wsb exec --id $id --command $command --run-as $runAs
+
+$command = "CiTool --refresh"
+echo "> $command"
+wsb exec --id $id --command $command --run-as $runAs
+
 $command = "robocopy C:\winconfig-readonly C:\winconfig /s /xf .* /xd .*"
 echo "> $command"
 wsb exec --id $id --command $command --run-as $runAs
