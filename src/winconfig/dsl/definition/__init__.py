94ef7e147269ffb378c2c2cc2f79e24b69b8e5bd
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-12-20 10:19:04 +0900

    refactor: move main logic from __init__.py to definition.py

--- src/winconfig/dsl/definition/__init__.py
+++ src/winconfig/dsl/definition/__init__.py
@@ -1,12 +1,3 @@
-from typing import Self
-
-from pydantic import (
-    BaseModel,
-    ConfigDict,
-    Field,
-    RootModel,
-)
-
 from .const_types import (  # noqa: F401
     ACCESS_DENIED,
     EXIST,
@@ -14,65 +5,17 @@ from .const_types import (  # noqa: F401
     NOT_EXIST,
     PERMISSION_DENIED,
 )
+from .definition import (  # noqa: F401
+    DefinitionBody,
+    DefinitionConfig,
+    DefinitionGroupName,
+    DefinitionName,
+)
 from .registry import (  # noqa: F401
     RegistryEntryDefinition,
     RegistryPathDefinition,
     RegistryValueKind,
 )
 from .schtask import SchtaskDefinition, SchtaskState  # noqa: F401
-from .script import ScriptDefinition
+from .script import ScriptDefinition  # noqa: F401
 from .service import ServiceDefinition, ServiceStartupType  # noqa: F401
-
-
-class DefinitionBody(BaseModel):
-    """A single, self-contained configuration task."""
-
-    description: str = Field(description="A description of the task's purpose.")
-    registries: list[RegistryPathDefinition] = Field(
-        default=[],
-        description="The registry values to be modified.",
-    )
-    scheduled_tasks: list[SchtaskDefinition] = Field(
-        default=[],
-        description="The scheduled tasks to be modified.",
-    )
-    services: list[ServiceDefinition] = Field(
-        default=[],
-        description="The Windows services to be modified.",
-    )
-    script: ScriptDefinition = Field(
-        default=ScriptDefinition(),
-        description="Custom PowerShell scripts for actions not covered by registry, services, or scheduled tasks.",
-    )
-
-    model_config = ConfigDict(
-        # use_enum_values=False,
-        extra="forbid",
-        json_schema_extra={
-            "anyOf": [
-                {"required": ["registries"]},
-                {"required": ["scheduled_tasks"]},
-                {"required": ["services"]},
-                {"required": ["script"]},
-            ]
-        },
-    )
-
-
-type DefinitionGroupName = str
-type DefinitionName = str
-
-
-class DefinitionConfig(RootModel):
-    root: dict[DefinitionGroupName, dict[DefinitionName, DefinitionBody]] = {}
-
-    @classmethod
-    def merge(cls, definition_configs: list[Self]) -> Self:
-        merged: dict[DefinitionGroupName, dict[DefinitionName, DefinitionBody]] = {}
-        for definition_config in definition_configs:
-            for group_name, group in definition_config.root.items():
-                if group_name not in merged:
-                    merged[group_name] = {}
-                merged[group_name] |= group
-
-        return cls.model_validate(merged)
---
+++ src/winconfig/dsl/definition/definition.py
@@ -0,0 +1,67 @@
+from typing import Self
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    RootModel,
+)
+
+from .registry import RegistryPathDefinition
+from .schtask import SchtaskDefinition
+from .script import ScriptDefinition
+from .service import ServiceDefinition
+
+
+class DefinitionBody(BaseModel):
+    """A single, self-contained configuration task."""
+
+    description: str = Field(description="A description of the task's purpose.")
+    registries: list[RegistryPathDefinition] = Field(
+        default=[],
+        description="The registry values to be modified.",
+    )
+    scheduled_tasks: list[SchtaskDefinition] = Field(
+        default=[],
+        description="The scheduled tasks to be modified.",
+    )
+    services: list[ServiceDefinition] = Field(
+        default=[],
+        description="The Windows services to be modified.",
+    )
+    script: ScriptDefinition = Field(
+        default=ScriptDefinition(),
+        description="Custom PowerShell scripts for actions not covered by registry, services, or scheduled tasks.",
+    )
+
+    model_config = ConfigDict(
+        # use_enum_values=False,
+        extra="forbid",
+        json_schema_extra={
+            "anyOf": [
+                {"required": ["registries"]},
+                {"required": ["scheduled_tasks"]},
+                {"required": ["services"]},
+                {"required": ["script"]},
+            ]
+        },
+    )
+
+
+type DefinitionGroupName = str
+type DefinitionName = str
+
+
+class DefinitionConfig(RootModel):
+    root: dict[DefinitionGroupName, dict[DefinitionName, DefinitionBody]] = {}
+
+    @classmethod
+    def merge(cls, definition_configs: list[Self]) -> Self:
+        merged: dict[DefinitionGroupName, dict[DefinitionName, DefinitionBody]] = {}
+        for definition_config in definition_configs:
+            for group_name, group in definition_config.root.items():
+                if group_name not in merged:
+                    merged[group_name] = {}
+                merged[group_name] |= group
+
+        return cls.model_validate(merged)
