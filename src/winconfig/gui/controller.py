76525a7e95c060e9d4406399eade2a2a41492d21
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-12-24 18:56:20 +0900

    feat(gui): add export button

--- src/winconfig/gui/controller.py
+++ src/winconfig/gui/controller.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import json
 import subprocess
 from pathlib import Path
 from textwrap import dedent
 from typing import TYPE_CHECKING
 
+import yaml
 from textual.containers import HorizontalGroup
 from textual.message import Message
 from textual.widgets import Button, Log
@@ -21,6 +23,7 @@ class TaskListController(HorizontalGroup):
     def compose(self) -> ComposeResult:
         yield RunButton()
         yield ImportButton()
+        yield ExportButton()
 
 
 class LogListController(HorizontalGroup):
@@ -87,6 +90,49 @@ class ImportButton(Button, RootAccessMixin):
         self.root.engine = Engine()
         self.root.engine.config.merge_from_yaml(Path(path))
         self.post_message(self.Imported())
+
+
+class ExportButton(Button, RootAccessMixin):
+    def __init__(self) -> None:
+        super().__init__(
+            "Export",
+            variant="primary",
+            flat=True,
+        )
+
+    def on_button_pressed(self, _: Button.Pressed) -> None:
+        result = subprocess.run(  # noqa: S603
+            [
+                "powershell",
+                "-Command",
+                dedent(
+                    """
+                    Add-Type -TypeDefinition @"
+                        using System.Runtime.InteropServices;
+                        public class DPIAware {
+                            [DllImport("user32.dll")]
+                            public static extern bool SetProcessDPIAware();
+                        }
+                    "@
+                    [DPIAware]::SetProcessDPIAware() | Out-Null
+                    Add-Type -AssemblyName System.Windows.Forms
+                    $dialog = New-Object System.Windows.Forms.SaveFileDialog
+                    $dialog.Filter = 'yaml files (*.yaml)|*.yaml|All files (*.*)|*.*'
+                    $dialog.FileName = 'winconfig.config.yaml'
+                    $dialog.ShowDialog() | Out-Null
+                    $dialog.FileName
+                    """
+                ),
+            ],
+            check=False,
+            capture_output=True,
+            text=True,
+        )
+        path = result.stdout.strip()
+
+        Path(path).write_text(
+            yaml.safe_dump(json.loads(self.root.engine.config.model_dump_json()))
+        )
 
 
 class BackButton(Button, RootAccessMixin):
